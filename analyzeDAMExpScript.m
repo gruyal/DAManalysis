2853be38cae06a37dad1ba0e72af9bcf6b218b59
AnreiterLab <ina.anreiter@utoronto.ca>
2026-08-21 12:23:55 -0400

    Update analyzeDAMExpScript.m
    Cleaned up the bout functions, added additional comments.

--- analyzeDAMExpScript.m
+++ analyzeDAMExpScript.m
@@ -1,13 +1,13 @@
 %% Analyzing experiment script
 
-dirName = '/Users/eyalgruntman/Library/CloudStorage/OneDrive-UniversityofToronto/Documents/DAManalysis';
+dirName = pwd;
 
 combDAMTab = processDAMDataFolder(dirName);
 
 %% adding metadata
 
-expMon = [69,67,68];
-monDesc = {'WT', 'mettle3D', 'something else'};
+expMon = [51, 52, 53, 54, 59, 60, 61];
+monDesc = {'A', 'B', 'C', 'D', 'E', 'F', 'H'};
 
 combDAMTab = addingMetaData2DAMTable(combDAMTab, expMon, monDesc);
 % 
@@ -17,63 +17,52 @@ combDAMTab = addingMetaData2DAMTable(combDAMTab, expMon, monDesc);
 % 
 clear expMon monDesc 
 
+%% Plots double actograms for sequential days in the experiment
+%% variable 2 specifies number and order of monitors to plot (e.g., 67:69 or [67,69], it will plot the monitors in order specified in variable 2)
+%% variable 3 specifies days to exclude at the end (e.g., "4" cuts day for and onward)
+
+plotMeanActivityFromMonitor(combDAMTab, [51, 52, 53, 54, 59, 60, 61])
+
+%% Plots sleep over ZT for sequential days in the experiment 
+%% variable 2 specifies number and order of monitors to plot (e.g., 67:69 or [67,69], it will plot the monitors in order specified in variable 2)
+%% variable 3 specifies days to exclude at the end (e.g., "4" cuts day 4 and onward)
+
+plotMeanSleepFromMonitor(combDAMTab, [51, 52, 53, 54, 59, 60, 61])
+
+
+%% Plots mean actogram for all days 
+%% variable 2 specifies monitors to plot (e.g., 67:69 or [67,69]) it will plot the monitors in order specified in variable 2
+%% variable 3 specifies days to plot (e.g., 1:5 or [1,4])
+
+plotMeanActivityOverDays(combDAMTab, [51, 52, 53, 54, 59, 60, 61], 1:4)
+
+%% Plots mean sleep ZT plot for all days 
+%% variable 2 specifies monitors to plot (e.g., 67:69 or [67,69]) it will plot the monitors in order specified in variable 2
+%% variable 3 specifies days to plot (e.g., 1:5 or [1,4])
+
+plotMeanSleepOverDays(combDAMTab, [51, 52, 53, 54, 59, 60, 61], 1:4)
+
+%% Plots total activity per 24hrs, averaged over days, as a boxplot 
+%% variable 2 specifies night day or total: 0=night, 1=day, 2=total
+%% variable 3 specifies days to plot (e.g., 1:5 or [1,4])
+%% if variable 3 is specified, variable 2 is needed, if variable 3 is blank leave variable 2 blank if plotting total (day and night)
+
+plotBoxplotMeanDayActivityByMon(combDAMTab, 1)
+
+%%
+%% Make the sleep version of 'plotBoxplotMeanDayActivityByMon'
 %%
 
-plotMeanActivityFromMonitor(combDAMTab, [67, 69], 5)
 
-%%
-
-plotMeanSleepFromMonitor(combDAMTab, 67:69)
-
-
-%%
-
-
-plotMeanActivityOverDays(combDAMTab, 67:69, 1:5)
-
-%%
-
-plotBoxplotMeanActivityByMon(combDAMTab)
-
-%%
-
-plotBoxplotMeanDayActivityByMon(combDAMTab,2, [2,3,5])
-
-
-%%
+%% Generates the sleep bout table
 
 sleepTab = generateSleepBoutTable(combDAMTab);
 
-%%
 
-bH = boxchart(sleepTab.RelZtHr, sleepTab.SleepBtLen, 'GroupByColor', sleepTab.MonOrd);
-for ii=1:length(bH)
-    bH(ii).MarkerStyle = '.';
-end
-
-figure
-
-bH = boxchart(sleepTab.RelZtHr, sleepTab.LogLen, 'GroupByColor', sleepTab.MonOrd);
-for ii=1:length(bH)
-    bH(ii).MarkerStyle = '.';
-end
-
-%%
-
-tempSlpTab = sleepTab(sleepTab.MonOrd == 1, :);
-
-bH = boxchart(tempSlpTab.FlyInd, tempSlpTab.SleepBtLen, 'GroupByColor', tempSlpTab.Light);
-for ii=1:length(bH)
-    bH(ii).MarkerStyle = '.';
-end
-
-%%
-
-bH = boxchart(sleepTab.Light, sleepTab.LogLen, 'GroupByColor', sleepTab.MonOrd);
-for ii=1:length(bH)
-    bH(ii).MarkerStyle = '.';
-end
-
-%%
+%% Plots average bout length and number per 24hrs in light and dark
 
 plotSleepBoutDurAndNumByLD(sleepTab,1)
+
+%% Plots average bout length and number over ZT time
+
+plotSleepBoutDurAndNumByZT(sleepTab,1)
